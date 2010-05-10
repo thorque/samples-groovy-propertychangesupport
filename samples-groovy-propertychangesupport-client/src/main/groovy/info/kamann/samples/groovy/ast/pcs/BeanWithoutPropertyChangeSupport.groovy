7749017b85d04289d36bab5aacc1fd573720bb6c
Thorsten Kamann <thorsten.kamann@googlemail.com>
2010-05-10 23:01:55 +0200

    Initial Import

---
+++ samples-groovy-propertychangesupport-client/src/main/groovy/info/kamann/samples/groovy/ast/pcs/BeanWithoutPropertyChangeSupport.groovy
@@ -0,0 +1,60 @@
+package info.kamann.samples.groovy.ast.pcs
+
+
+/**
+ * @author kamann
+ */
+
+@WithPropertySupport
+public class BeanWithoutPropertyChangeSupport {
+	private String name;
+	private String street;
+	private String city;
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+		
+	}
+
+	/**
+	 * @return the street
+	 */
+	public String getStreet() {
+		return street;
+	}
+
+	/**
+	 * @param street
+	 *            the street to set
+	 */
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city
+	 *            the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+}
---
+++ samples-groovy-propertychangesupport-client/src/test/groovy/info/kamann/samples/groovy/ast/pcs/PTest.groovy
@@ -0,0 +1,43 @@
+package info.kamann.samples.groovy.ast.pcs;
+
+import groovyjarjarasm.asm.Opcodes;
+
+import java.beans.PropertyChangeEvent 
+import java.beans.PropertyChangeListener;
+import java.util.Map;
+
+import org.codehaus.groovy.ast.ASTNode 
+import org.codehaus.groovy.ast.builder.AstBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+class PTest {
+	private PropertyChangeListener listener
+	private Map result = [name:"", oldValue:"", newValue:""]
+	
+	@Before
+	final void setup(){
+		listener = [propertyChange:{PropertyChangeEvent e -> 
+			result.name = e.propertyName
+			result.oldValue = e.oldValue
+			result.newValue = e.newValue
+		}] as PropertyChangeListener
+		
+	}
+	
+
+	@Test
+	final void SimpleTest(){
+		BeanWithoutPropertyChangeSupport bean = new BeanWithoutPropertyChangeSupport()
+		bean.addPropertyChangeListener( listener)
+		
+		
+		println bean.support.listeners
+		
+		bean.setName "jjjj"
+		println "$result.name $result.oldValue $result.newValue"
+	}
+
+}
